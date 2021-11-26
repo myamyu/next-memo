2db562c31393c819ce1af3ac70ffcb1c1787277f
myamyu <myamyu@gmail.com>
2021-11-27 01:50:20 +0900

    フロント作り始め

---
+++ next-memo-front/components/memos.tsx
@@ -0,0 +1,20 @@
+// TODO swrを使ってメモを取得する https://swr.vercel.app/ja/docs/getting-started
+const Memos = () => {
+  return (
+    <ul>
+      <li>
+        めも１
+      </li>
+      <li>
+        めも２
+      </li>
+      <li>
+        めも３
+      </li>
+    </ul>
+  )
+};
+
+export {
+  Memos,
+};
---
+++ next-memo-front/pages/api/memo.ts
@@ -0,0 +1,3 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+// TODO メモの操作をするAPIを作る
