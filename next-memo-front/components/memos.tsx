5791ee46d0f27f4090459330e1a73ab7c1d1a06d
myamyu <myamyu@gmail.com>
2021-12-01 01:03:55 +0900

    メモをSWRでAPIから取得するようにしてみた

--- next-memo-front/components/memos.tsx
+++ next-memo-front/components/memos.tsx
@@ -1,16 +1,36 @@
-// TODO swrを使ってメモを取得する https://swr.vercel.app/ja/docs/getting-started
+import React from "react";
+import useSWR from "swr";
+
+const fetcher = (url: string) => {
+  return fetch(url)
+    .then((res: Response) => {
+      return res.json();
+    });
+};
+
+// 改行を<br>に
+const LfToBr = (text:string) => text
+  .split('\n')
+  .map((s, i) => <React.Fragment key={i}>{i === 0 ? '' : <br />}{s}</React.Fragment>);
+
 const Memos = () => {
+  const {data, error} = useSWR('/api/memo/', fetcher)
+  if (error) return <div>えらーだ！！！</div>
+  if (!data) return <div>ロード中…</div>
+  const memos = data as Memo[];
+
   return (
     <ul>
-      <li>
-        めも１
-      </li>
-      <li>
-        めも２
-      </li>
-      <li>
-        めも３
-      </li>
+      {
+        memos.map((memo) => {
+          return (
+            <li key={memo.id}>
+              <h3>{memo.title}</h3>
+              <p>{LfToBr(memo.description)}</p>
+            </li>
+          );
+        })
+      }
     </ul>
   )
 };
--- next-memo-front/pages/api/memo/index.ts
+++ next-memo-front/pages/api/memo/index.ts
@@ -1,16 +1,24 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Handler = (req: NextApiRequest, res: NextApiResponse) => void;
+type Handler = (req: NextApiRequest, res: NextApiResponse) => Promise<void>;
 const methods:{[key: string]:Handler} = {
-  'get': (req: NextApiRequest, res: NextApiResponse<Memo[]>) => {
+  'get': async (req: NextApiRequest, res: NextApiResponse<Memo[]>) => {
+    await (
+      new Promise<void>((resolve) => {
+        setTimeout(() => {
+          resolve();
+        }, 800);
+      })
+    );
     res.status(200)
       .json([
         {id: 0, title: 'めもめも', description: 'あいうえお\nかきくけこ'},
         {id: 1, title: 'めもめも2', description: 'あいうえお\nかきくけこ'},
         {id: 2, title: 'めもめも3', description: 'あいうえお\nかきくけこ'},
+        {id: 3, title: 'めもめも3', description: 'あいうえお,かきくけこ'},
       ]);
   },
-  'post': (req: NextApiRequest, res: NextApiResponse<Memo>) => {
+  'post': async (req: NextApiRequest, res: NextApiResponse<Memo>) => {
     res.status(200)
       .json(
         {id: 0, title: 'めもめも', description: 'あいうえお\nかきくけこ'},
@@ -18,12 +26,12 @@ const methods:{[key: string]:Handler} = {
   },
 }
 
-export default function handler(req: NextApiRequest, res:NextApiResponse<Memo[]|Memo|void>) {
+export default async function handler(req: NextApiRequest, res:NextApiResponse<Memo[]|Memo|void>) {
   const method = methods[(req.method || '').toLowerCase()];
   if (!method) {
     res.status(404).json();
     return;
   }
 
-  method(req, res);
+  await method(req, res);
 }
